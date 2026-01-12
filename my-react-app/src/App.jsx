8aa87228b294aeca4c49f83ba73826ed60800816
SamuelSgn25 <soglohounsamuel2@gmail.com>
2026-01-12 18:30:41 +0100

    Add routes for navigation

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,6 +4,10 @@ import FAQ from './Pages/FAQ'
 import Contact from './Pages/Contact'
 import Auth from './Pages/Authentification'
 import './index.css'
+import Dashboard from './Pages/Dashboard'
+import Clients from './Pages/Clients'
+import Services from './Pages/Services'
+import Rapports from './Pages/Rapports'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -16,6 +20,10 @@ function App() {
           <Route path="/faq" element={<FAQ />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/auth" element={<Auth />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/clients' element={<Clients/>}/>
+          <Route path='/services' element={<Services/>}/>
+          <Route path='/reports' element={<Rapports/>}/>
         </Routes>
       </BrowserRouter>
     </div>
