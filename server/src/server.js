4c61a34bf6b835185ce142c43d75376fd1779a0a
SamuelSgn25 <soglohounsamuel2@gmail.com>
2026-03-02 10:18:19 +0100

    Modify data for backend server

--- server/src/server.js
+++ server/src/server.js
@@ -47,8 +47,8 @@ const limiter = rateLimit({
 app.use('/api/', limiter);
 
 // Body parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 
 // Routes
 app.use('/api', routes);
