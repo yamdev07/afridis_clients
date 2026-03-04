5f24ca713f3fdec09ceea3293c653bcec90ced60
SamuelSgn25 <soglohounsamuel2@gmail.com>
2026-03-04 11:55:42 +0100

    Add packages and tailwind configuration

--- my-react-app/tailwind.config.js
+++ my-react-app/tailwind.config.js
@@ -1,11 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#2563EB',
+          dark: '#1D4ED8',
+        },
+        accent: {
+          green: '#10B981',
+          purple: '#8B5CF6',
+          orange: '#F59E0B',
+        }
+      },
+      borderRadius: {
+        '18': '18px',
+      }
+    },
   },
   plugins: [],
 }
