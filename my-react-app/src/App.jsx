fe876881ecaef81af6eaf085d317babe243acdb1
SamuelSgn25 <soglohounsamuel2@gmail.com>
2026-03-04 11:10:29 +0100

    Add new routes

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Home from './Pages/Home'
 import FAQ from './Pages/FAQ'
 import Contact from './Pages/Contact'
-import Auth from './Pages/Authentification'
 import Dashboard from './Pages/Dashboard'
 import Clients from './Pages/Clients'
 import Services from './Pages/Services'
@@ -17,23 +16,23 @@ import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 function App() {
 
   return (
-    <div>
+    <div className="transition-colors duration-300">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/faq" element={<FAQ />} />
           <Route path="/contact" element={<Contact />} />
-          {/* Ancienne page combinée (connexion / inscription) redirigée vers la connexion */}
           <Route path="/auth" element={<Navigate to="/login" replace />} />
+          <Route path="/register" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
           <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/clients' element={<Clients/>}/>
-          <Route path='/services' element={<Services/>}/>
-          <Route path='/reports' element={<Rapports/>}/>
-          <Route path='/import' element={<Import/>}/>
-          <Route path='/export' element={<Export/>}/>
-          <Route path='/profile' element={<Profile/>}/>
-          <Route path='/admin/users' element={<AdminUsers/>}/>
+          <Route path='/clients' element={<Clients />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/reports' element={<Rapports />} />
+          <Route path='/import' element={<Import />} />
+          <Route path='/export' element={<Export />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/admin/users' element={<AdminUsers />} />
         </Routes>
       </BrowserRouter>
     </div>
