f5d1d55edd945bf46714630672023fd26f2d063f
SamuelSgn25 <soglohounsamuel2@gmail.com>
2026-03-06 10:04:23 +0100

    Cleaning useless information

--- server/src/config/jwt.js
+++ server/src/config/jwt.js
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  secret: process.env.JWT_SECRET || 'change_this_secret_in_production',
-  refreshSecret: process.env.JWT_REFRESH_SECRET || 'change_this_refresh_secret_in_production',
-  expiresIn: process.env.JWT_EXPIRES_IN || '15m',
-  refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '7d',
+  secret: process.env.JWT_SECRET,
+  refreshSecret: process.env.JWT_REFRESH_SECRET,
+  expiresIn: process.env.JWT_EXPIRES_IN,
+  refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
 };
