eed5665b2eb56325d5595bb8e33f457f385b1ba7
SamuelSgn25 <soglohounsamuel2@gmail.com>
2026-03-11 09:29:11 +0100

    Implementing notifications routes

--- server/src/routes/notificationRoutes.js
+++ server/src/routes/notificationRoutes.js
@@ -3,6 +3,7 @@ import {
   listNotifications,
   markNotificationRead,
   markAllNotificationsRead,
+  deleteNotification
 } from '../controllers/notificationController.js';
 import { authenticateToken } from '../middlewares/auth.js';
 
@@ -14,6 +15,7 @@ router.get('/', listNotifications);
 // Route littérale avant paramétrée pour éviter que "mark-all-read" soit pris pour :id
 router.patch('/mark-all-read', markAllNotificationsRead);
 router.patch('/:id/read', markNotificationRead);
+router.delete('/:id', deleteNotification);
 
 export default router;
 
